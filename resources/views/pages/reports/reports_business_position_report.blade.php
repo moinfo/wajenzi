c6640ced2c00d8a0d6d85b065df7101031bf3087
mohamed amiry <kibahaonline@gmail.com>
2021-05-01 17:38:51 +0300

    Fix date in reports

--- resources/views/pages/reports/reports_business_position_report.blade.php
+++ resources/views/pages/reports/reports_business_position_report.blade.php
@@ -86,10 +86,6 @@
                                 $inventory = \App\Models\Report::getTotalInventoryForSpecificDate($start_date,$end_date);
                                 $capital = \App\Models\Report::getTotalCapitalForSpecificDate($start_date,$end_date);
                                 $supplier_credit = \App\Models\Report::getTotalSupplierBalance($end_date);
-                                $supplier_credit1 = \App\Models\SupplierReceiving::getAllSupplierReceivingAmount($end_date);
-                                $supplier_credit2 = \App\Models\TransactionMovement::getAllSupplierTransactionAmount($end_date);
-                                dump($supplier_credit1);
-                                dump($supplier_credit2);
                                 $total_in = $cash + $credit + $inventory;
                                 $total_out = $supplier_credit + $capital;
                                 $difference = $total_in - $total_out
