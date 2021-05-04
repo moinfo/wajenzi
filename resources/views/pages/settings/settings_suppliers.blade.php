4bd2422512142ad5d0d14c7635d29e3d454d68c8
mohamed amiry <kibahaonline@gmail.com>
2021-05-04 19:25:49 +0300

    Fix few changes on setting

--- resources/views/pages/settings/settings_suppliers.blade.php
+++ resources/views/pages/settings/settings_suppliers.blade.php
@@ -49,7 +49,7 @@
                             @foreach($suppliers as $supplier)
                                 <tr id="supplier-tr-{{$supplier->id}}">
                                     <td class="text-center">
-                                        {{$item->id}}
+                                        {{$supplier->id}}
                                     </td>
                                     <td class="font-w600">{{ $supplier->name }}</td>
                                     <td class="font-w400">{{ $supplier->phone }}</td>
