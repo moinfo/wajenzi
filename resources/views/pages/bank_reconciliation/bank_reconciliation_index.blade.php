9213f70b4286e2775e45accd32647adc59f35f54
mohamed amiry <kibahaonline@gmail.com>
2022-05-13 13:04:49 +0300

    fix on bank reconciliation

--- resources/views/pages/bank_reconciliation/bank_reconciliation_index.blade.php
+++ resources/views/pages/bank_reconciliation/bank_reconciliation_index.blade.php
@@ -147,9 +147,9 @@
                                         @endphp
                                         <tr>
                                             <td colspan="2" class="text-right">KIWANDANI</td>
-                                            <td class="text-right">{{number_format($deposit_out_only,2)}}</td>
-                                            <td class="text-right">{{number_format(($deposit_out_only)-($deposit_excluded_out+$deposit_excluded_out_only),2)}}</td>
-                                            <td class="text-right">{{number_format(($deposit_excluded_out+$deposit_excluded_out_only),2)}}</td>
+                                            <td class="text-right">{{number_format($deposit_out,2)}}</td>
+                                            <td class="text-right">{{number_format(($deposit_out)-($deposit_excluded_out),2)}}</td>
+                                            <td class="text-right">{{number_format(($deposit_excluded_out),2)}}</td>
                                         </tr>
                                         </tfoot>
                                     </table>
