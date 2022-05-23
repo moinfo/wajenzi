ffc4597ae324332c2ceeb7aba320192c9644d432
mohamed amiry <kibahaonline@gmail.com>
2022-05-23 21:55:46 +0300

    fix on total supplier

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -94,7 +94,7 @@ class ReportsController extends Controller
         return view('pages.reports.reports_supervisor_report')->with($data);
     }
     public function total_credit_suppliers_report(Request $request){
-        $suppliers_with_bonge = Supplier::where('supplier_depend_on_system','=','BONGE')->where('is_transferred','!=','CAN BE BOTH')->orderBy('supplier_depend_on_system', 'DESC')->get();
+        $suppliers_with_bonge = Supplier::where('supplier_depend_on_system','=','BONGE')->where('id','!=','88')->where('is_transferred','!=','CAN BE BOTH')->orderBy('supplier_depend_on_system', 'DESC')->get();
         $suppliers_with_whitestar = Supplier::where('supplier_depend_on_system','=','WHITESTAR')->where('id','!=','52')->where('is_transferred','!=','CAN BE BOTH')->orderBy('supplier_depend_on_system', 'DESC')->get();
 
         $data = [
