feebe9174bc2dcdd6cab0adab6b3a1621c8b0919
mohamed amiry <kibahaonline@gmail.com>
2022-08-29 13:28:29 +0300

    Quick Fix on bank report by reverse date updated

--- resources/views/pages/reports/reports_bank_report.blade.php
+++ resources/views/pages/reports/reports_bank_report.blade.php
@@ -66,14 +66,14 @@
                                     $dates[] = $date->format("Y-m-d");
                                 }
                                 ?>
-                                @foreach($dates as $date)
+                                @foreach(array_reverse($dates) as $date)
                                   @php
                                       $yesterday = date('Y-m-d', strtotime('-1 day', strtotime($date)));
                                         $turnover = \App\Models\Sale::getTotalTurnover($date,$date,null);
                                         $bank_deposit = \App\Models\BankReconciliation::getTotalDepositPerSupplierBank($date,$date);
                                         $difference = $turnover - $bank_deposit;
-                                        $all_time_turnover = \App\Models\Sale::getTotalTurnover($date_from_began,$yesterday,null);
-                                        $all_time_bank_deposit = \App\Models\BankReconciliation::getTotalDepositPerSupplierBank($date_from_began,$yesterday,null);
+                                        $all_time_turnover = \App\Models\Sale::getTotalTurnover($date_from_began,$date,null);
+                                        $all_time_bank_deposit = \App\Models\BankReconciliation::getTotalDepositPerSupplierBank($date_from_began,$date,null);
                                         $all_time_difference = $all_time_turnover - $all_time_bank_deposit;
 
 
