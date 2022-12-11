5b6c909abe13aaf76fa55a9f2a576a00504daba3
mohamed amiry <kibahaonline@gmail.com>
2022-12-11 14:22:14 +0300

    fix on user groups

--- resources/views/pages/settings/settings_user_groups.blade.php
+++ resources/views/pages/settings/settings_user_groups.blade.php
@@ -4,77 +4,61 @@
     <div class="main-container">
         <div class="content">
             <div class="content-heading">Settings
+{{--                @include('components.headed_paper_settings')--}}
+                <br/>
+                <div class="block-header text-center">
+                    <h3 class="block-title">User Groups</h3>
+                </div>
                 <div class="float-right">
-                    @if(\App\Models\UsersPermission::isUserAllowed(Auth::user()->id,"CRUD","Add Advance Salary"))
-                        <button type="button" onclick="loadFormModal('settings_advance_salary_form', {className: 'AdvanceSalary'}, 'Create New AdvanceSalary', 'modal-md');" class="btn btn-rounded btn-outline-primary min-width-125 mb-10">
-                            <i class="si si-plus">&nbsp;</i>New AdvanceSalary</button> @endif
+                    @if(\App\Models\UsersPermission::isUserAllowed(Auth::user()->id,"CRUD","Add User Group"))
+                        <button type="button" onclick="loadFormModal('settings_user_group_form', {className: 'UserGroup'}, 'Create New User Group', 'modal-md');" class="btn btn-rounded btn-outline-primary min-width-125 mb-10">
+                            <i class="si si-plus">&nbsp;</i>New User Group</button> @endif
 
                 </div>
             </div>
             <div>
 
                 <div class="block">
-                    <div class="block-header block-header-default">
-                        <h3 class="block-title">Advanced Salaries</h3>
-                    </div>
+
                     <div class="block-content">
                         <table class="table table-bordered table-striped table-vcenter js-dataTable-full">
-                        <thead>
+                            <thead>
                             <tr>
                                 <th class="text-center" style="width: 100px;">#</th>
                                 <th>Date</th>
                                 <th>Name</th>
-                                <th>Description</th>
-                                <th>Amount</th>
+                                <th>Keyword</th>
                                 <th class="text-center" style="width: 100px;">Actions</th>
                             </tr>
                             </thead>
                             <tbody>
-                            <?php
-                            $sum = 0;
-                            ?>
-                            @foreach($advance_salaries as $advance_salary)
-                                <?php
-                                    $sum += $advance_salary->amount;
-                                ?>
-                                <tr id="advance_salary-tr-{{$advance_salary->id}}">
-                                    <td class="text-center">
-                                        {{$loop->index + 1}}
-                                    </td>
-                                    <td class="font-w600">{{ $advance_salary->date }}</td>
-                                    <td class="font-w600">{{ $advance_salary->staff->name  ?? null}}</td>
-                                    <td class="font-w600">{{ $advance_salary->description}}</td>
-                                    <td class="text-right">{{ number_format($advance_salary->amount) }}
-                                    </td>
+                            @foreach ($user_groups as $key => $value)
+                                <tr>
+                                    <th scope="row">
+                                        <a href="#">{{$loop->iteration}}</a>
+                                    </th>
+                                    <td>{{ $value->updated_at }}</td>
+                                    <td>{{ $value->name }}</td>
+                                    <td>{{ $value->keyword }}</td>
                                     <td class="text-center" >
                                         <div class="btn-group">
-                                            @if(\App\Models\UsersPermission::isUserAllowed(Auth::user()->id,"CRUD","Edit Advance Salary"))
-                                                <button type="button" onclick="loadFormModal('settings_advance_salary_form', {className: 'AdvanceSalary', id: {{$advance_salary->id}}}, 'Edit {{$advance_salary->name}}', 'modal-md');" class="btn btn-sm btn-primary js-tooltip-enabled" data-toggle="tooltip" title="Edit" data-original-title="Edit">
+                                            @if(\App\Models\UsersPermission::isUserAllowed(Auth::user()->id,"CRUD","Edit User Group"))
+                                                <button type="button" onclick="loadFormModal('settings_user_group_form', {className: 'UserGroup', id: {{$value->id}}}, 'Edit {{$value->name}}', 'modal-md');" class="btn btn-sm btn-primary js-tooltip-enabled" data-toggle="tooltip" title="Edit" data-original-title="Edit">
                                                     <i class="fa fa-pencil"></i>
                                                 </button>
                                             @endif
 
-                                                @if(\App\Models\UsersPermission::isUserAllowed(Auth::user()->id,"CRUD","Delete Advance Salary"))
-                                                    <button type="button" onclick="deleteModelItem('AdvanceSalary', {{$advance_salary->id}}, 'advance_salary-tr-{{$advance_salary->id}}');" class="btn btn-sm btn-danger js-tooltip-enabled" data-toggle="tooltip" title="Delete" data-original-title="Delete">
-                                                        <i class="fa fa-times"></i>
-                                                    </button>
-                                                @endif
+                                            @if(\App\Models\UsersPermission::isUserAllowed(Auth::user()->id,"CRUD","Delete User Group"))
+                                                <button type="button" onclick="deleteModelItem('UserGroup', {{$value->id}}, 'user_group-tr-{{$value->id}}');" class="btn btn-sm btn-danger js-tooltip-enabled" data-toggle="tooltip" title="Delete" data-original-title="Delete">
+                                                    <i class="fa fa-times"></i>
+                                                </button>
+                                            @endif
 
                                         </div>
                                     </td>
                                 </tr>
                             @endforeach
                             </tbody>
-                            <tfoot>
-                                <tr>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td class="text-right">{{number_format($sum)}}</td>
-                                    <td></td>
-                                </tr>
-                            </tfoot>
                         </table>
                     </div>
                 </div>
