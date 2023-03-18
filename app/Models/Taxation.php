c3df777e079b8fb4a51c45bb1c44c35a6c802140
mohamed amiry <kibahaonline@gmail.com>
2023-03-18 11:18:46 +0300

    fix on comprenhensive update

--- app/Models/Taxation.php
+++ app/Models/Taxation.php
@@ -20,33 +20,38 @@ class Taxation extends Model
     }
     public static function getMainlandTaxation($start_date,$end_date){
         $Profit_from_Operating_Activities_Before_Taxation_current = self::ProfitFromOperatingActivitiesBeforeTaxation($start_date,$end_date);
-        $employee_percentage = 0;
-        $additional_amount = 0;
-        $maximum_amount = 0;
-        if($Profit_from_Operating_Activities_Before_Taxation_current >= 0 && $Profit_from_Operating_Activities_Before_Taxation_current < 3240000){
-            $employee_percentage = 0;
-            $additional_amount = 0;
-            $maximum_amount = 3240000;
-        }elseif($Profit_from_Operating_Activities_Before_Taxation_current >= 3240000 && $Profit_from_Operating_Activities_Before_Taxation_current < 6240000){
-            $employee_percentage = 0.09;
-            $additional_amount = 0;
-            $maximum_amount = 6240000;
-        }elseif($Profit_from_Operating_Activities_Before_Taxation_current >= 6240000 && $Profit_from_Operating_Activities_Before_Taxation_current < 9120000){
-            $employee_percentage = 0.2;
-            $additional_amount = 270000;
-            $maximum_amount = 9120000;
-        }elseif($Profit_from_Operating_Activities_Before_Taxation_current >= 9120000 && $Profit_from_Operating_Activities_Before_Taxation_current < 12000000){
-            $employee_percentage = 0.25;
-            $additional_amount = 846000;
-            $maximum_amount = 120000000;
-        }elseif($Profit_from_Operating_Activities_Before_Taxation_current >= 12000000){
-            $employee_percentage = 0.30;
-            $additional_amount = 1566000;
-            $maximum_amount = 12000000;
-        }
-        return $Profit_from_Operating_Activities_Before_Taxation_current*0.30;
+//        $employee_percentage = 0;
+//        $additional_amount = 0;
+//        $maximum_amount = 0;
+//        if($Profit_from_Operating_Activities_Before_Taxation_current >= 0 && $Profit_from_Operating_Activities_Before_Taxation_current < 3240000){
+//            $employee_percentage = 0;
+//            $additional_amount = 0;
+//            $maximum_amount = 3240000;
+//        }elseif($Profit_from_Operating_Activities_Before_Taxation_current >= 3240000 && $Profit_from_Operating_Activities_Before_Taxation_current < 6240000){
+//            $employee_percentage = 0.09;
+//            $additional_amount = 0;
+//            $maximum_amount = 6240000;
+//        }elseif($Profit_from_Operating_Activities_Before_Taxation_current >= 6240000 && $Profit_from_Operating_Activities_Before_Taxation_current < 9120000){
+//            $employee_percentage = 0.2;
+//            $additional_amount = 270000;
+//            $maximum_amount = 9120000;
+//        }elseif($Profit_from_Operating_Activities_Before_Taxation_current >= 9120000 && $Profit_from_Operating_Activities_Before_Taxation_current < 12000000){
+//            $employee_percentage = 0.25;
+//            $additional_amount = 846000;
+//            $maximum_amount = 120000000;
+//        }elseif($Profit_from_Operating_Activities_Before_Taxation_current >= 12000000){
+//            $employee_percentage = 0.30;
+//            $additional_amount = 1566000;
+//            $maximum_amount = 12000000;
+//        }
+//        return $Profit_from_Operating_Activities_Before_Taxation_current*0.30;
 //        return ($additional_amount + $employee_percentage* ($maximum_amount - $Profit_from_Operating_Activities_Before_Taxation_current));
-
+        if($Profit_from_Operating_Activities_Before_Taxation_current < 0){
+            $tax = 0;
+        }else{
+            $tax = $Profit_from_Operating_Activities_Before_Taxation_current * 0.3;
+        }
+        return $tax;
     }
 
     public static function Profit_From_Operating_Activities_After_Taxation($start_date,$end_date){
