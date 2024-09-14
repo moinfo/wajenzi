ff1999eb22242ae6d83d9fdea2421114dd1388c7
mohamed amiry <info@moinfo.co.tz>
2024-09-14 11:47:03 +0300

    mix

--- resources/views/pages/bank_reconciliation/bank_reconciliation_index.blade.php
+++ resources/views/pages/bank_reconciliation/bank_reconciliation_index.blade.php
@@ -6,7 +6,7 @@
         <div class="content">
             <div class="content-heading">BR
                 <div class="float-right">
-                    <a href="{{route('bank_deposit_report')}}" class="btn btn-rounded btn-outline-danger min-width-100 mb-10"><i class="si si-graph">&nbsp;</i>BP</a>
+                    <a href="{{route('bank_deposit_report')}}" class="btn btn-rounded btn-outline-danger min-width-100 mb-10"><i class="si si-graph">&nbsp;</i>BD</a>
                     <a href="{{route('supplier_targets')}}" class="btn btn-rounded btn-outline-warning min-width-100 mb-10"><i class="si si-graph">&nbsp;</i>ST</a>
                     <a href="{{route('supplier_targets_report')}}" class="btn btn-rounded btn-outline-warning min-width-100 mb-10"><i class="si si-graph">&nbsp;</i>STR</a>
                     <a href="{{route('transfer_reports')}}" class="btn btn-rounded btn-outline-success min-width-100 mb-10"><i class="si si-graph">&nbsp;</i>TRR</a>
@@ -471,10 +471,9 @@
                                 </tbody>
                                 <tfoot>
                                 <tr>
-                                    <td colspan="8"></td>
+                                    <td colspan="9"></td>
                                     <td class="text-right">{{ number_format($total_credit, 2) }}</td>
                                     <td class="text-right">{{ number_format($total_debit, 2) }}</td>
-                                    <td></td>
                                     <td></td>
                                 </tr>
                                 </tfoot>
