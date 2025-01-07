17aa15a6148014185ce2a6b4a0aba02543f3319f
mohamed amiry <info@moinfo.co.tz>
2025-01-07 16:52:30 +0300

    fix on add supplier

--- resources/views/forms/settings_supplier_form.blade.php
+++ resources/views/forms/settings_supplier_form.blade.php
@@ -1,6 +1,5 @@
-
 <div class="block-content">
-    <form  method="post"  autocomplete="off">
+    <form method="post" autocomplete="off">
         @csrf
         <div class="form-group">
             <label for="example-nf-email" class="control-label required">Name</label>
@@ -8,24 +7,8 @@
         </div>
         <div class="form-group">
             <label for="example-nf-phone">Phone</label>
-            <input type="text" class="form-control" id="input-phone" name="phone" value="{{ $object->phone ?? '' }}" placeholder="Phone Number" >
+            <input type="text" class="form-control" id="input-phone" name="phone" value="{{ $object->phone ?? '' }}" placeholder="Phone Number">
         </div>
-{{--        <div class="form-group" >--}}
-{{--            <label for="example-nf-account_name">Account Name</label>--}}
-{{--            <input type="text" class="form-control" id="input-account_name" name="account_name" value="{{ $object->account_name ?? '' }}" placeholder="Account Name" >--}}
-{{--        </div>--}}
-{{--        <div class="form-group" >--}}
-{{--            <label for="example-nf-crdb_account">CRDB Account</label>--}}
-{{--            <input type="text" class="form-control" id="input-crdb_account" name="crdb_account" value="{{ $object->crdb_account ?? '' }}" placeholder="CRDB Account" >--}}
-{{--        </div>--}}
-{{--        <div class="form-group" >--}}
-{{--            <label for="example-nf-nmb_account">NMB Account</label>--}}
-{{--            <input type="text" class="form-control" id="input-nmb_account" name="nmb_account" value="{{ $object->nmb_account ?? '' }}" placeholder="NMB Account" >--}}
-{{--        </div>--}}
-{{--        <div class="form-group" >--}}
-{{--            <label for="example-nf-nbc_account">NBC Account</label>--}}
-{{--            <input type="text" class="form-control" id="input-nbc_account" name="nbc_account" value="{{ $object->nbc_account ?? '' }}" placeholder="NBC Account" >--}}
-{{--        </div>--}}
         <div class="form-group">
             <label for="example-nf-address">Address</label>
             <input type="text" class="form-control" id="input-address" name="address" value="{{ $object->address ?? '' }}" placeholder="Address">
@@ -55,48 +38,58 @@
                 @endforeach
             </select>
         </div>
+
         @if($object->supplier_depend_on_system)
             @if($object->supplier_depend_on_system == 'WHITESTAR')
-        <div class="form-group">
-            <label for="example-nf-system" class="control-label required">Whitestar Supplier</label>
-            <select name="whitestar_supplier_id" id="input-whitestar_supplier-id" class="form-control" >
-                <option value="">Select Whitestar Supplier</option>
-                @foreach ($whitestar_suppliers as $whitestar_supplier)
-                    <option value="{{ $whitestar_supplier->local_supplier_id }}" {{ ( $whitestar_supplier->local_supplier_id == $object->whitestar_supplier_id) ? 'selected' : '' }}> {{ $whitestar_supplier->first_name . ' '. $whitestar_supplier->last_name }} </option>
-                @endforeach
-            </select>
-        </div>
+                <div class="form-group">
+                    <label for="example-nf-system" class="control-label required">Whitestar Supplier</label>
+                    <select name="whitestar_supplier_id" id="input-whitestar_supplier-id" class="form-control">
+                        <option value="">Select Whitestar Supplier</option>
+                        @foreach ($whitestar_suppliers as $whitestar_supplier)
+                            <option value="{{ $whitestar_supplier->local_supplier_id }}" {{ ( $whitestar_supplier->local_supplier_id == $object->whitestar_supplier_id) ? 'selected' : '' }}>
+                                {{ $whitestar_supplier->first_name . ' '. $whitestar_supplier->last_name }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
             @else
-        <div class="form-group">
-            <label for="example-nf-system" class="control-label required">Bonge Supplier</label>
-            <select name="whitestar_supplier_id" id="input-bonge_supplier-id" class="form-control" >
-                <option value="">Select Bonge Supplier</option>
-                @foreach ($bonge_suppliers as $bonge_supplier)
-                    <option value="{{ $bonge_supplier->local_supplier_id }}" {{ ( $bonge_supplier->local_supplier_id == $object->whitestar_supplier_id) ? 'selected' : '' }}> {{ $bonge_supplier->first_name . ' '. $bonge_supplier->last_name }} </option>
-                @endforeach
-            </select>
-        </div>
+                <div class="form-group">
+                    <label for="example-nf-system" class="control-label required">Bonge Supplier</label>
+                    <select name="bonge_supplier_id" id="input-bonge_supplier-id" class="form-control">
+                        <option value="">Select Bonge Supplier</option>
+                        @foreach ($bonge_suppliers as $bonge_supplier)
+                            <option value="{{ $bonge_supplier->local_supplier_id }}" {{ ( $bonge_supplier->local_supplier_id == $object->bonge_supplier_id) ? 'selected' : '' }}>
+                                {{ $bonge_supplier->first_name . ' '. $bonge_supplier->last_name }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
             @endif
         @else
             <div class="form-group" id="whitestar" style='display:none;'>
                 <label for="example-nf-system" class="control-label required">Whitestar Supplier</label>
-                <select name="whitestar_supplier_id" id="input-whitestar_supplier-id" class="form-control" >
+                <select name="whitestar_supplier_id" id="input-whitestar_supplier-id" class="form-control">
                     <option value="">Select Whitestar Supplier</option>
                     @foreach ($whitestar_suppliers as $whitestar_supplier)
-                        <option value="{{ $whitestar_supplier->local_supplier_id }}" {{ ( $whitestar_supplier->local_supplier_id == $object->whitestar_supplier_id) ? 'selected' : '' }}> {{ $whitestar_supplier->first_name . ' '. $whitestar_supplier->last_name }} </option>
+                        <option value="{{ $whitestar_supplier->local_supplier_id }}" {{ ( $whitestar_supplier->local_supplier_id == $object->whitestar_supplier_id) ? 'selected' : '' }}>
+                            {{ $whitestar_supplier->first_name . ' '. $whitestar_supplier->last_name }}
+                        </option>
                     @endforeach
                 </select>
             </div>
             <div class="form-group" id="bonge" style='display:none;'>
                 <label for="example-nf-system" class="control-label required">Bonge Supplier</label>
-                <select name="whitestar_supplier_id" id="input-bonge_supplier-id" class="form-control" >
+                <select name="bonge_supplier_id" id="input-bonge_supplier-id" class="form-control">
                     <option value="">Select Bonge Supplier</option>
                     @foreach ($bonge_suppliers as $bonge_supplier)
-                        <option value="{{ $bonge_supplier->local_supplier_id }}" {{ ( $bonge_supplier->local_supplier_id == $object->bonge_supplier_id) ? 'selected' : '' }}> {{ $bonge_supplier->first_name . ' '. $bonge_supplier->last_name }} </option>
+                        <option value="{{ $bonge_supplier->local_supplier_id }}" {{ ( $bonge_supplier->local_supplier_id == $object->bonge_supplier_id) ? 'selected' : '' }}>
+                            {{ $bonge_supplier->first_name . ' '. $bonge_supplier->last_name }}
+                        </option>
                     @endforeach
                 </select>
             </div>
-            @endif
+        @endif
+
         <div class="form-group">
             <label for="example-nf-system" class="control-label required">System</label>
             <select name="system_id" id="input-system-id" class="form-control" required>
@@ -131,20 +124,57 @@
         </div>
     </form>
 </div>
+
 <script>
     $('.datepicker').datepicker({
         format: 'yyyy-mm-dd'
     });
-    $('#supplier_depend_on_system').on('change', function() {
-        if (this.value == 'WHITESTAR')
-        {
+
+    // Initialize form based on initial value
+    $(document).ready(function() {
+        const initialSystem = $('#supplier_depend_on_system').val();
+        if (initialSystem == 'WHITESTAR') {
             $("#whitestar").show();
             $("#bonge").hide();
-        }else
-        {
+            $("#input-bonge_supplier-id").prop('disabled', true);
+            $("#input-whitestar_supplier-id").prop('disabled', false);
+        } else if (initialSystem == 'BONGE') {
             $("#bonge").show();
             $("#whitestar").hide();
+            $("#input-whitestar_supplier-id").prop('disabled', true);
+            $("#input-bonge_supplier-id").prop('disabled', false);
+        } else {
+            $("#whitestar").hide();
+            $("#bonge").hide();
+            $("#input-whitestar_supplier-id").prop('disabled', true);
+            $("#input-bonge_supplier-id").prop('disabled', true);
         }
     });
 
+    // Handle system change
+    $('#supplier_depend_on_system').on('change', function() {
+        // Reset both select values when changing system
+        $('#input-whitestar_supplier-id').val('');
+        $('#input-bonge_supplier-id').val('');
+
+        if (this.value == 'WHITESTAR') {
+            $("#whitestar").show();
+            $("#bonge").hide();
+            // Disable Bonge supplier field when Whitestar is selected
+            $("#input-bonge_supplier-id").prop('disabled', true);
+            $("#input-whitestar_supplier-id").prop('disabled', false);
+        } else if (this.value == 'BONGE') {
+            $("#bonge").show();
+            $("#whitestar").hide();
+            // Disable Whitestar supplier field when Bonge is selected
+            $("#input-whitestar_supplier-id").prop('disabled', true);
+            $("#input-bonge_supplier-id").prop('disabled', false);
+        } else {
+            $("#whitestar").hide();
+            $("#bonge").hide();
+            // Disable both when nothing is selected
+            $("#input-whitestar_supplier-id").prop('disabled', true);
+            $("#input-bonge_supplier-id").prop('disabled', true);
+        }
+    });
 </script>
