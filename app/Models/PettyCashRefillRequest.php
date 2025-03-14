a393bdf689c68366ff267aede0adbb0c9166cbab
mohamed amiry <info@moinfo.co.tz>
2025-03-14 17:57:00 +0300

    implemented petty cash refill

---
+++ app/Models/PettyCashRefillRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PettyCashRefillRequest extends Model
+{
+    use HasFactory;
+
+    protected $table = 'petty_cash_refill_requests';
+
+    protected $fillable = ['document_number','balance','refill_amount','status','create_by_id','file','date'];
+
+    public function user(){
+        return $this->belongsTo(User::class,'create_by_id');
+    }
+
+}
